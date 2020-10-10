1ce5ec565629973e0b9cdbc2ee8a052a280b68b9
liteobsidian <liteobsidian@gmail.com>
2020-10-11 01:47:21 +0300

    Updates

---
+++ precache-manifest.dd2d543042c8dc8b022365f63770cdb8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5972d959b367f39c03a5bae5018fe609",
+    "url": "/first/index.html"
+  },
+  {
+    "revision": "bc6fea0f1e8e0a7149a2",
+    "url": "/first/static/css/3.ccb78997.chunk.css"
+  },
+  {
+    "revision": "aa843a2709716e349ca6",
+    "url": "/first/static/css/4.76564f37.chunk.css"
+  },
+  {
+    "revision": "ef6ead18970b31b703e4",
+    "url": "/first/static/css/main.e2f6adf3.chunk.css"
+  },
+  {
+    "revision": "bd5d67963ac64a960ecb",
+    "url": "/first/static/js/2.d8d59c82.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/first/static/js/2.d8d59c82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc6fea0f1e8e0a7149a2",
+    "url": "/first/static/js/3.3227c03a.chunk.js"
+  },
+  {
+    "revision": "aa843a2709716e349ca6",
+    "url": "/first/static/js/4.82934a61.chunk.js"
+  },
+  {
+    "revision": "ef6ead18970b31b703e4",
+    "url": "/first/static/js/main.4b7774d8.chunk.js"
+  },
+  {
+    "revision": "66ccab31ff26123a0806",
+    "url": "/first/static/js/runtime-main.06db8936.js"
+  },
+  {
+    "revision": "9708d09f574199fbaac9c01470caee2a",
+    "url": "/first/static/media/load.9708d09f.png"
+  },
+  {
+    "revision": "8abe9bfae4eed8dde305263e40504edc",
+    "url": "/first/static/media/team00.8abe9bfa.png"
+  },
+  {
+    "revision": "d380cc81695dde169705d093f1965490",
+    "url": "/first/static/media/user.d380cc81.png"
+  },
+  {
+    "revision": "ced290a69965e8ddb5bcf4d6ca3db4ae",
+    "url": "/first/static/media/wall.ced290a6.jpg"
+  }
+]);
